866791f5c2536c20d3f16ec829903a983defe6ae
VopRos-ZP <zolpavel366@gmail.com>
2023-09-11 19:52:14 +0300

    added ex.5

--- src/main/kotlin/sixExercises/Five.kt
+++ src/main/kotlin/sixExercises/Five.kt
@@ -1,5 +1,23 @@
 package sixExercises
 
-fun main() {
+import utils.InputLoop
+import java.util.Scanner
+import kotlin.math.pow
 
+fun main() {
+    InputLoop.start(error = "Неверный ввод") {
+        with(Scanner(System.`in`)) {
+            print("Введите целое число n: ")
+            val num = nextInt()
+            print("Введите целое основание степени x: ")
+            val base = nextInt()
+            for (i in 1..<num + 1) {
+                if (i.toDouble().pow(base).toInt() == num) {
+                    println("y = $i")
+                    return@start
+                }
+            }
+            println("Целочисленного показателя степени нет")
+        }
+    }
 }
