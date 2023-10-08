ed16c17cf82031b8727c49080e673f073c83d973
VopRos-ZP <zolpavel366@gmail.com>
2023-10-08 18:12:54 +0300

    added `Six`

--- src/main/kotlin/sixExercises/Six.kt
+++ src/main/kotlin/sixExercises/Six.kt
@@ -1,5 +1,24 @@
 package sixExercises
 
-fun main() {
+import utils.InputLoop
+import java.util.*
 
+fun main() {
+    InputLoop.start(error = "Неверный ввод") {
+        with(Scanner(System.`in`)) {
+            print("Введите целое число a: ")
+            val a = nextInt()
+            print("Введите целое число b: ")
+            val b = nextInt()
+            println(
+                when {
+                    isSumOdd(a, b) -> a * 10 + b
+                    isSumOdd(b, a) -> b * 10 + a
+                    else -> "Создать нечетное число невозможно"
+                }
+            )
+        }
+    }
 }
+
+private fun isSumOdd(a: Int, b: Int): Boolean = (a * 10 + b).mod(2) == 1
